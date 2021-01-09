d3ab98dbbe150d98d2dd6b64b080e32f28e9b760
victorbruce <victorbruce82@gmail.com>
2021-01-09 22:55:26 +0000

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import StackNavigator from './navigation/Stack';
-// import TabBottomNavigator from './navigation/TabBottom';
 import DrawerNavigator from './navigation/Drawer';
 
 function App() {
   return (
     <NavigationContainer>
-      {/* <TabBottomNavigator /> */}
       <DrawerNavigator />
     </NavigationContainer>
   );
