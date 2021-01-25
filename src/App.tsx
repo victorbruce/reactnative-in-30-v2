6369c55aeaffb2fbe3c9c76589c8daddf7bb07cb
victorbruce <victorbruce82@gmail.com>
2021-01-25 01:22:38 +0000

    feat: implement sign up, sign in and sign out functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 import AuthNavigator from './navigation/AuthStack';
 import AppNavigator from './navigation/AppStack';
 
 function App() {
-  const [user, setUser] = useState<boolean>(true);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(fbuser: any) {
+    setUser(fbuser);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
 
   return (
     <NavigationContainer>
