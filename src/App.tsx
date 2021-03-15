e8efc5ad3dd9df5cad6f69d9fae4845e2cc4a51d
victorbruce <victorbruce82@gmail.com>
2021-03-15 03:58:20 +0000

    fix issue with firebase library

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import auth from '@react-native-firebase/auth';
-
-import AuthNavigator from './navigation/AuthStack';
-import AppNavigator from './navigation/AppStack';
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
 function App() {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-
-  function onAuthStateChanged(fbuser: any) {
-    setUser(fbuser);
-    if (initializing) {
-      setInitializing(false);
-    }
-  }
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
-  }, []);
-
-  if (initializing) {
-    return null;
-  }
-
   return (
-    <NavigationContainer>
-      {user ? <AppNavigator /> : <AuthNavigator />}
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
